a0a9c725bf17fc0188b8263d615e9453ea675c10
Alexey Krutikov <alex-krutikov@yandex.ru>
2016-01-11 00:08:49 +0300

    v3

--- SkypeVideoDownloader.qbs
+++ SkypeVideoDownloader.qbs
@@ -1,10 +1,14 @@
 import qbs
 
 Application {
+    Depends { name: "cpp" }
     Depends { name: "Qt.widgets" }
     Depends { name: "Qt.sql" }
 
+    cpp.cxxPrecompiledHeader: "pch.h"
+
     files: [
+        "pch.h",
         "main.cpp",
         "mainwindow.ui",
     ]
--- main.cpp
+++ main.cpp
@@ -15,6 +15,114 @@ public:
 namespace {
 }
 
+struct ModelItem
+{
+  enum Status {Unknown = 0, Ready, InProgress, Finished, Error };
+
+  ModelItem();
+
+  QString id;
+  QString vod_path;
+  Status status;
+
+};
+
+ModelItem::ModelItem()
+  : status(Unknown)
+{
+}
+
+class Model : public QAbstractTableModel
+{
+public:
+  Model(QObject *parent = 0);
+  int rowCount(const QModelIndex &parent = QModelIndex()) const;
+  int columnCount(const QModelIndex &parent = QModelIndex()) const;
+  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+  QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+
+  void reset();
+  void addItem(const QString &id, const QString &vod_path);
+
+private:
+  QVector<ModelItem> items;
+};
+
+Model::Model(QObject *parent)
+  : QAbstractTableModel(parent)
+{
+}
+
+int Model::rowCount(const QModelIndex &parent) const
+{
+  Q_UNUSED(parent);
+  return items.count();
+}
+
+int Model::columnCount(const QModelIndex &parent) const
+{
+  Q_UNUSED(parent);
+  return 3;
+}
+
+QVariant Model::data(const QModelIndex &index, int role) const
+{
+  switch (role)
+  {
+    case Qt::DisplayRole:
+      switch (index.column())
+      {
+      case 0:
+          return items.at(index.row()).id;
+      case 1:
+          return items.at(index.row()).vod_path;
+      case 2:
+          return items.at(index.row()).vod_path;
+
+      }
+
+  }
+  return QVariant();
+}
+
+QVariant Model::headerData(int section, Qt::Orientation orientation, int role) const
+{
+  if (orientation == Qt::Vertical) {
+    if (role == Qt::DisplayRole )
+      return section + 1;
+    return QVariant();
+  }
+
+  switch (role)
+  {
+    case Qt::DisplayRole:
+    switch (section) {
+    case 0:
+      return "id";
+    case 1:
+      return "status";
+    case 2:
+      return "vod_path";
+    }
+  }
+  return QVariant();
+}
+
+void Model::reset()
+{
+  beginResetModel();
+  items.clear();
+  endResetModel();
+}
+
+void Model::addItem(const QString &id, const QString &vod_path)
+{
+  ModelItem item;
+  item.id = id;
+  item.vod_path = vod_path;
+  items << item;
+}
+
 class MainWindow : public QWidget
 {
   Q_OBJECT
@@ -30,6 +138,7 @@ private slots:
   void onGetVideosClicked();
 private:
   Ui::MainWindow ui;
+  Model *model;
 };
 
 MainWindow::MainWindow(QWidget *parent)
@@ -40,6 +149,9 @@ MainWindow::MainWindow(QWidget *parent)
   connect(ui.pb_getVideos, SIGNAL(clicked(bool)), this, SLOT(onGetVideosClicked()));
 
   readSettings();
+
+  model = new Model(this);
+  ui.tv->setModel(model);
 }
 
 void MainWindow::writeSettings()
@@ -94,10 +206,14 @@ void MainWindow::onGetVideosClicked()
         ui.label_statusline->setText("Cannot query video path from database.");
         return;
     }
+
+    model->reset();
+
     while (query.next()) {
         const QString id = query.value(0).toString();
         const QString vodPath = query.value(1).toString();
-        qDebug() << id << vodPath;
+        //qDebug() << id << vodPath;
+        model->addItem(id, vodPath);
     }
 
 
